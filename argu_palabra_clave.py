7264b01ab418aac240a7b5f74a3e5721103c7757
Rafael Fenoy Rivas <rafaelfenoyrivas@gmail.com>
2025-12-14 16:16:43 +0100

    actualizacion

--- argu_palabra_clave.py
+++ argu_palabra_clave.py
@@ -1,11 +1,3 @@
-# Argumentos de palabra clave.
-
-def describe_pet(animal_type, pet_name):
-    """Muestra información sobre una mascota."""
-    print(f"\nI have a {animal_type}.")
-    print(f"My {animal_type}'s name is {pet_name}")
-
-describe_pet(animal_type= 'hamster', pet_name='harry')
 
 # Con argumentos de palabra clave, el orden no importa porque cada valor
 # se asigna explícitamente a su parámetro. 
@@ -14,11 +6,38 @@ describe_pet(animal_type= 'hamster', pet_name='harry')
 
 # Solo pasamos pet_name. animal_type tomará el valor predeterminado ('dog').
 def describe_pet(pet_name, animal_type='dog'):
-    print(f"\nI have a {animal_type}.")
+    if pet_name == 'willie 1':
+        print('1º')
+    else:
+        print('2º')
+    print(f"I have a {animal_type}.")
     print(f"My {animal_type}'s name is {pet_name.capitalize()}")
-describe_pet(pet_name='willie')
-
+describe_pet(pet_name='willie 1')
 # Con dos valores en argumento.
-describe_pet(pet_name='willie', animal_type='dog')
+describe_pet(pet_name='willie 2', animal_type='dog')
+print("-" * 40)
+
+# Argumentos de palabra clave.
+def describe_pet(animal_type, pet_name):
+    """Muestra información sobre una mascota."""
+    print(f"3º\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name}")
+
+describe_pet(animal_type= 'hamster', pet_name='harry')
+
+print("-" * 40)
+
+# Valores de retorno.
+# Devolver solo un valor:
+# formated_name.py
+def get_formated_name(first_name, last_name):
+    """Devuelve un nombre completo, con formato adecuado"""
+    full_name = f"4º\n{first_name} {last_name}"
+    return full_name.title()
+
+musician = get_formated_name('jimi', 'hendrix')
+print(musician)
+
+print("-" * 40)
 
 
--- ejercicios_8.py
+++ ejercicios_8.py
@@ -5,7 +5,7 @@ def mostrar_mensaje():
     print("En este capítulo estamos aprendiendo funciones.")
 
 mostrar_mensaje()
-
+print("-" * 40)
 
 # Ejercicio 8_2. Libro favorito.
 
@@ -14,24 +14,35 @@ def libro_favorito(titulo):
     print(f"Mi libro favorito es: {titulo.capitalize()}")
 
 libro_favorito('viven')
+print("-" * 40)
 
 # Ejercicio 8-3. Camiseta.
-
 def camiseta(talla, texto):
     """Haz tu propia camiseta indicando talla y texto"""
-    print(f"\nVoy a fabricar una camiseta de la talla: {talla}.")
-    print(f"El texto es: '{texto}'")
+    print(f"\nVoy a fabricar una camiseta de la talla: 👕 {talla}.")
+    print(f"El texto es: \t'{texto}'")
 
 camiseta('XL', 'I love python')
+print("-" * 40)
 
 # Ejercicio 8-4. Camisetas grandes.
 """Camiseta grande"""
 def hacer_camiseta(talla, mensaje):
-    print(f"\nVoy a fabricar una camiseta de la talla:{talla}")
-    print(f"el texto será: '{mensaje}'")
+    print(f"\nVoy a fabricar una camiseta de la talla:👕 {talla}")
+    print(f"el texto será: '\t{mensaje}'")
 
 hacer_camiseta('XL', 'I love python')
-
 hacer_camiseta('M', 'I love python')
-
 hacer_camiseta('L', 'I love COMPUTER')
+print("-" * 40)
+
+# ejercicio 8-5. ciudades.
+def describir_ciudad(ciudad, pais='españa'):
+    print(f"\nLa ciudad: {ciudad.capitalize()}, está en: 🌍 {pais.capitalize()}")
+
+describir_ciudad('barcelona')
+describir_ciudad('madrid')
+describir_ciudad('murcia')
+describir_ciudad('dublín', 'irlanda')
+print("-" * 40)
+
